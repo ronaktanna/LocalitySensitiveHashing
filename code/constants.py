7366288ff57301b1f22357668f2adb260ee03858
ronaktanna <ronaktanna.personal@gmail.com>
2018-12-12 22:51:33 -0700

    Added constants file

---
+++ code/constants.py
@@ -0,0 +1,8 @@
+"""
+Constants file.
+"""
+MODELS = ["CM", "CM3x3", "CN", "CN3x3", "CSD", "GLRLM", "GLRLM3x3", "HOG", "LBP", "LBP3x3"]
+DEVSET_TOPICS_DIR_PATH = "../dataset/textual_descriptors/devset_topics.xml"
+VISUAL_DESCRIPTORS_DIR_PATH = "../dataset/visual_descriptors/"
+TEXT_DESCRIPTORS_DIR_PATH = "../dataset/textual_descriptors/"
+OUTPUT_FILE = "../output/output/task5_output.txt"
